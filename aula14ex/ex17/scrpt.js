e6adce793991d450327cca1f08bcd69212581340
Filipe <filipesilveira.dev@gmail.com>
2025-04-26 21:16:00 -0300

    exercício - contador

--- aula14ex/ex17/scrpt.js
+++ aula14ex/ex17/scrpt.js
@@ -1,1 +1,6 @@
-alert('Olá')
+function contar (){
+    var c = 1
+    for (var c = 1; c>=10; c++){
+        console.log(`Passo ${c}`)
+    }
+}    
--- aula14ex/modelo/scrpt.js
+++ aula14ex/modelo/scrpt.js
@@ -1,1 +1,10 @@
-alert('Olá')
+function contar(){
+    var inicio = window.document.getElementById('inicio')
+    var fim = window.document.getElementById('fim')
+    var cont = window.document.getElementById('cont')
+    var c =Number(cont.value)
+    var res = window.document.getElementById('res')
+
+    for((inicio.value); c<= (fim.value); c=++)
+    
+}
