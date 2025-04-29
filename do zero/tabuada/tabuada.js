593e3c5965e2e6faeca71f057879b151e1e34bd8
Filipe <filipesilveira.dev@gmail.com>
2025-04-29 11:40:13 -0300

    exercício tabuada do zero

---
+++ do zero/tabuada/tabuada.js
@@ -0,0 +1,26 @@
+function calcular(){
+    var num = document.getElementById("num")
+    var n = Number(num.value)
+    var res = document.getElementById('res')
+
+    if(num.value.length == 0){
+        window.alert('Digite um número!')
+    } 
+
+    res.innerHTML = ''
+    /*for(var i = 1; i<=10; i++){
+        res.innerHTML += `${n} x ${i} = ${n*i} <br>`
+        res.style.border = '1px, solid, black'
+        res.style.borderRadius = '10px'
+    }*/
+
+        for(var i = 1; i<=10; i++){
+            var item
+        }
+}
+
+function limpar(){
+    location.reload() // comando atualiza a página, limpanda o que havia antes
+    /*res.innerHTML = 'Digite um número acima'
+    res.style.border = 'none'*/
+}
