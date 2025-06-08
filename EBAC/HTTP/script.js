336191ae5cb5114d437fcbdd8a2beef054b54027
Filipe <filipesilveira.dev@gmail.com>
2025-06-07 21:54:36 -0300

    Início HTTP

---
+++ EBAC/HTTP/script.js
@@ -0,0 +1,9 @@
+//seleciona a <ul> com a lista de tarefas do HTML 
+const tarefas = document.getElementById("listaTarefas");
+
+//utilizar o fecth para pegar (GET) as tarefas criadas no Crudcrud no exercício com postman
+fetch("")
+    .then (response => response.json());
+    .then (listaDeTarefas) =>{              //
+
+    }
