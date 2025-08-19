f535f36b10f444c8e1fb8e7a8babefcf397171af
Filipe <filipesilveira.dev@gmail.com>
2025-08-18 22:37:31 -0300

    Exercício DELETE javascript não funcionando

--- EBAC/HTTP/script.js
+++ EBAC/HTTP/script.js
@@ -3,74 +3,73 @@ const tarefas = document.getElementById("listaTarefas");
 
 //obs: "descricao" é a chave e seu valor é "Tarefa 1"
 
-//utilizar o fecth para pegar (GET) as tarefas criadas no Crudcrud no exercício com postman
-fetch("https://cafe17c542449caddf3c.free.beeceptor.com/api/tarefas/")
-.then ((response) => response.json())
-.then ((listaDeTarefas) =>{       //se der certo, será retornada uma array com várias tarefas
+//utilizar o fecth para pegar (GET) as tarefas criadas no Crudcrud no exercício com postman. Por utilizar o "fetch", o "GET" não precisa ser especificado como nos outros casos.
+fetch("https://crudcrud.com/api/1c70734bdef74bfea3733b6258bf11c2/tarefas")
+    //caso a requisição dê certo, ou seja, a resposta venha, visa-se buscar o json dela
+    .then((response) => response.json())
+    //se der certo pegar o json da resposta, será retornada uma array com várias tarefas
+    .then((listaDeTarefas) => {
 
-    //itera sobre cada tarefa do array. Para cada elemento (tarefa) de listaDeTarefas será executada o que está na arrow function
-    listaDeTarefas.forEach(tarefa => { 
+        //itera sobre cada tarefa do array. Para cada elemento (tarefa) de listaDeTarefas será executada o que está na arrow function
+        listaDeTarefas.forEach(tarefa => {
 
-        //cria um novo elemento de lista (<li>) para cada tarefa
-        const item = document.createElement("li");
-        //define o conteúdo HTML do item, incluindo descrição e botão
-        item.innerHTML = `${tarefa.descricao} <button oclick="remove(${tarefa.id})">X</button>`;
-        //adiciona o novo item à lista de tarefas no HTML
-        tarefas.appendChild(item);
-    });
-})
-.catch(error => console.error("Erro ao adicionar tarefa: ", error)); 
+            //cria um novo elemento de lista (<li>) para cada tarefa
+            const item = document.createElement("li");
+            //define o conteúdo HTML do item, incluindo descrição e botão (o json se ordena "descricao":"valor". Nesse caso, está agindo na descrição, na chave). Adiciona-se também um botão
+            item.innerHTML = `${tarefa.descricao} <button onclick="remove(${tarefa._id})">X</button>`;
+            //adiciona o novo item à constante "tarefas" a qual recebe o elemento HTML com id = "listaTarefas"
+            tarefas.appendChild(item);
+        });
+    })
+    .catch(error => console.error("Erro ao adicionar tarefa: ", error));
 
 //criar o evento de cadastrar um nova atrefa
 document.getElementById("add").addEventListener("click", () => {
 
     //seleciona o elemento HTML (id="terefa") de onde será recebido o valor ou informação que será acrescentada na lista de tarefas 
-    const description = document.getElementById("tarefa").value;  
-    
+    const descricao = document.getElementById("tarefa").value;
+
     //a URL será a mesma utilizada anteriormente no método GET, porém ao final será acrecentado um objeto e suas informações que serão acrescidas
-    fetch("https://cafe17c542449caddf3c.free.beeceptor.com/api/tarefas/", {
+    fetch("https://crudcrud.com/api/1c70734bdef74bfea3733b6258bf11c2/tarefas", {
 
         method: "POST", //método de acrescentar
         headers: {
             //objeto informa que o tipo de conteúdo que será passado é do tipo JSON
-            "Content-Type" : "application/jason" 
+            "Content-Type": "application/json"
         },
         //no body é o que será enviado, no caso um arquivo JSON. Dessa forma é utilizado o parâmetro estabelecido no postman (descricao) que fica na primeira posição dos parâmetros passados e, no segundo parâmetro, será passada a variável "description" criada para receber o valor do que há no elemento HTML com id="tarefa". Como se trata de um objeto em javascript, é necessário transformá-lo em JSON por meio do JSON.stringify().
-        body: JSON.stringify({descricao: description}) 
+        body: JSON.stringify({ descricao: descricao })
 
     })
-    .then (response => response.json())
-    .then (tarefa => {
+        .then(response => response.json())
+        .then((tarefa) => {
 
-        //cria um novo elemento de lista (<li>) para cada tarefa
-        const item = document.createElement("li");
-        //define o conteúdo HTML do item, incluindo descrição e botão
-        item.innerHTML = `${tarefa.descricao} <button oclick="remove(${tarefa.id})">X</button>`;
-        //adiciona o novo item à lista de tarefas no HTML
-        tarefas.appendChild(item);
-    })
+            //cria um novo elemento de lista (<li>) para cada tarefa
+            const item = document.createElement("li");
+            //define o conteúdo HTML do item, incluindo descrição e botão
+            item.innerHTML = `${tarefa.descricao} <button onclick="remove(${tarefa._id})">X</button>`;
+            //adiciona o novo item à lista de tarefas no HTML
+            tarefas.appendChild(item);
+        })
 })
 
 
-function remove(tarefaId){
-    fetch(`https://cafe17c542449caddf3c.free.beeceptor.com/api/tarefas/${tarefaId}`, {
-        method: 'DELETE',
+function remove(tarefaId) {
+    fetch(`https://crudcrud.com/api/1c70734bdef74bfea3733b6258bf11c2/tarefas/${tarefaId}`, {
+        method: "DELETE",
         headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => {
-        if(response.ok){
-            alert ("Usuário deletado!");
-            response.json();
-        } else {
-            alert ("Não foi possível deletar o usuário")
-        }
-    })
-    .then (data =>{
-        if (data) {
-            console.log('Resposta:', data);
-        }
-    })
-    .catch(error => console.error("Erro ao deletar tarefa: ", error));
+            "Content-Type": "application/json"
+        },
+    });
+
+    // Verifica se deu certo
+    if (response.ok) {
+        console.log(`Item com ID ${tarefaId} foi deletado com sucesso!`);
+        return true;
+    } else {
+        console.log('Erro ao deletar item:', response.status);
+        return false;
+    }
+
+      
 }
