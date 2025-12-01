4ade5d57b8b61e2d622c4f320d53e9b304829ac5
Filipe <filipesilveira.dev@gmail.com>
2025-12-01 17:00:25 -0300

    Precisa resolver a questão do line-through

--- EBAC/10 - React/exercicios/curso-de-react-yt/src/App.jsx
+++ EBAC/10 - React/exercicios/curso-de-react-yt/src/App.jsx
@@ -1,15 +1,50 @@
+import { useState } from "react"
 import AddTask from "./components/AddTask"
 import Tasks from "./components/Tasks"
 
 function App() {
+const [tasks, setTask] = useState([{
+  id: 1,
+  title: "Estudar programação",
+  description: "Estudar programação para se tornar um desenvolvedor full stack.",
+  isCompleted: false
+},
+{
+   id: 2,
+  title: "Estudar inglês",
+  description: "Estudar inglês para se tornar fluente.",
+  isCompleted: false
+},
+{
+   id: 3,
+  title: "Estudar matemática",
+  description: "Estudar matemática para se tornar um desenvolvedor full stack.",
+  isCompleted: false
+}])
 
+
+function onTaskClick(taskId){
+  const newTasks = tasks.map((task)=>{
+    // PRECISO ATUALIZAR ESSA TAREFA
+    if(task.id === taskId){
+      return {...task, isCompleted: !task,isCompleted};
+
+    // NÃO PRECISO ATUALIZAR ESSA TAREFA
+    return task;
+    }
+  })
+}
   return (
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
-      {/* componente Tasks */}
-      <Tasks />
-      {/* componente AddTask */}
-      <AddTask />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+           {/* componente AddTask */}
+        <AddTask />
+        {/* componente Tasks */}
+        {/* tudo o que é passado aqui, é possível ter acesso no seu componente.jsx por meio de props. No exemplo a seguir, é dado acesso aos valores contidos na array "tasks" lá no componente Tasks*/}
+        {/* também é possível passar função como props */}
+        <Tasks tasks={tasks} onTaskClick={onTaskClick}/>
+      </div>
     </div>
   )
 }
--- EBAC/10 - React/exercicios/curso-de-react-yt/src/components/Tasks.jsx
+++ EBAC/10 - React/exercicios/curso-de-react-yt/src/components/Tasks.jsx
@@ -1,7 +1,27 @@
-function Tasks(){
-return(
-    <h1>Tasks</h1>
-)
+import { ChevronRightIcon } from "lucide-react"
+// sendo o argumento "props" é possível este componente acessar informações que estão "acima" dele lá no App.jsx. Ou seja, o componente "Tasks" é filho do "App", pois é renderizado por este.
+function Tasks(props) {
+    return (
+        // aqui é feito um map(), forEach, para cada elemento presente na array "tasks" com useState, exibindo uma lista
+        // lembrando que o uso da "key" é essencial em listas no react
+        <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+            {props.tasks.map((task) =>
+                <li key={task.id} className="flex gap-2">
+                    {/* arrow function executada ao ser clicado onde a função "onTaskClick", com o argumento da id da tarefa clicada */}
+                    <button 
+                    onClick={()=>props.onTaskClick(task.id)} 
+                    className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted && "line-through"}`}>
+                        {task.title}
+                        </button>
+
+                    <button className="bg-slate-400 p-2 rounded-md text-white">
+                        {/* componente obtido por meio do "npm install lucide-react@0.435.0" */}
+                        <ChevronRightIcon />
+                    </button>
+                </li>
+            )}
+        </ul>
+    )
 }
 
 export default Tasks
